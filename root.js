3fbc5192f30ba5f97bfb1d3015da42dd4c7f842b
Leedabord <gwforumlakers@gmail.com>
2016-04-29 10:15:07 -0700

    2-way binding using ngModel

--- root.js
+++ root.js
@@ -1,10 +1,15 @@
-//gwfl 1.a  add "root" with empty array to angular.module
+//gwfl 0.1.a  add "root" with empty array to angular.module
 angular.module('root', [])
 
-//gwfl 2.a  add "index" controller with $scope
+//gwfl 0.2.a  add "index" controller with $scope
     .controller("index", ["$scope", function ($scope) {
 
-//gwfl 3.a  set ".message" in $scope
+//gwfl 0.3.a  set ".message" in $scope
         $scope.message = "Hello World!";
-
+        
+//gwfl 1.1.a  bind to html view using ngModel
+        $scope.favoriteWord;
+        $scope.favoriteColor;
+        $scope.favoriteShape;
+        
     }]);
