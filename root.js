dd1e4199ea8397dde81aff4de81b4ccf36698207
Leedabord <gwforumlakers@gmail.com>
2016-04-29 11:43:11 -0700

    Update root.js

--- root.js
+++ root.js
@@ -11,5 +11,11 @@ angular.module('root', [])
         $scope.favoriteWord;
         $scope.favoriteColor;
         $scope.favoriteShape;
+
+//gwfl 1.2.a  ng-class applies class if returns true
+		$scope.value = 1;
+		$scope.isBold = function () { return $scope.value % 2 === 0; };
+		$scope.isItalic = function () { return $scope.value % 3 === 0; };
+		$scope.isUnderlined = function () { return $scope.value % 5 === 0; };
         
     }]);
