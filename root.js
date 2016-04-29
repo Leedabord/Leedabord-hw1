4e6972cff99f3f3759b66ae139ccb0fbec298834
Leedabord <gwforumlakers@gmail.com>
2016-04-29 09:57:46 -0700

    updated comments  //gwfl

--- root.js
+++ root.js
@@ -1,10 +1,10 @@
-// 1.a  add "root" with empty array to angular.module
+//gwfl 1.a  add "root" with empty array to angular.module
 angular.module('root', [])
 
-// 2.a  add "index" controller with $scope
+//gwfl 2.a  add "index" controller with $scope
     .controller("index", ["$scope", function ($scope) {
 
-// 3.a  set ".message" in $scope
+//gwfl 3.a  set ".message" in $scope
         $scope.message = "Hello World!";
 
     }]);
